2806d32cce4c1038118672957eb65cd90b042262
William <williamallen12364@gmail.com>
2021-08-30 21:27:50 +0700

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>hello boys</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import router from './routes/posts.js'
+
+const app = express()
+
+app.use('/posts', router)
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://joy:joy123@memoriesdb.ovsyr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message))
+
+// To not get warnings in the 
+// mongoose.set('useFindAndModify', false);
